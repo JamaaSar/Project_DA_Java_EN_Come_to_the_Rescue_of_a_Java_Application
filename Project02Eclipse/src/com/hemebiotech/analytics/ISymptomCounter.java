86c8b7db9c969087eee45c383b264c806bef8edf
Jamaa Saranbat <jamaasaranbat@macbook-pro-de-jamaa.home>
2022-09-22 13:31:12 +0200

    create interfaces

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomCounter.java
@@ -0,0 +1,17 @@
+package com.hemebiotech.analytics;
+
+
+/**
+ * 
+ * Anything that will count occurence of a symptom from a givin source
+ * Will return a object that maps keys (name of the symptom) to values (occurence) 
+ * 
+ */
+
+public interface ISymptomCounter {
+
+	
+
+	
+
+}
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
@@ -0,0 +1,15 @@
+package com.hemebiotech.analytics;
+
+
+/**
+ * 
+ * Create a file with given file name and write results in that file  
+ * 
+ */
+
+public interface ISymptomWriter {
+	
+	
+
+
+}
